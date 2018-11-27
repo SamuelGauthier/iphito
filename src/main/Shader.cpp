f33973c49cba285fff818d3f3b14866c5e9015c9
Samuel Gauthier <samuel.luc.gauthier@gmail.com>
2018-11-27 19:43:05 +0100

    add shader class

---
+++ src/main/Shader.cpp
@@ -0,0 +1,117 @@
+/**
+ * @file Shader.cpp
+ * @brief Implements a shader
+ * @author Samuel Gauthier
+ * @version 0.1.0
+ * @date 2018-11-21
+ */
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <sstream>
+#include <GL/glew.h>
+
+#include "Shader.h"
+
+#include "Utils.h"
+#include "Logger.h"
+
+Shader::Shader(std::string vertexShaderPath, std::string fragmentShaderPath) {
+
+    if(!Utils::isGlfwInitialized())
+        throw std::runtime_error("Please initialize GLFW.");
+
+    this->vertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+    this->fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+
+    std::string vertexShaderCode = Utils::readFile(vertexShaderPath);
+    std::string fragmentShaderCode = Utils::readFile(fragmentShaderPath);
+
+    this->compileShader(vertexShaderID, vertexShaderCode, "vertex shader (" +
+                        vertexShaderPath + ")");
+    this->compileShader(fragmentShaderID, fragmentShaderCode, "fragment shader"
+                        " (" + fragmentShaderPath + ")");
+
+    Logger::Instance()->info("Linking program");
+
+    this->shaderProgramID = glCreateProgram();
+    glAttachShader(this->shaderProgramID, this->vertexShaderID);
+    glAttachShader(this->shaderProgramID, this->fragmentShaderID);
+    glLinkProgram(this->shaderProgramID);
+
+
+    GLint linkStatus = 0;
+    int infoLogLength = 0;
+
+    glGetProgramiv(this->shaderProgramID, GL_LINK_STATUS, &linkStatus);
+    glGetProgramiv(this->shaderProgramID, GL_INFO_LOG_LENGTH, &infoLogLength);
+
+    if(infoLogLength > 0) {
+
+        std::vector<char> shaderErrorMessage(infoLogLength + 1);
+        glGetProgramInfoLog(this->shaderProgramID, infoLogLength, NULL,
+                            &shaderErrorMessage[0]);
+        std::stringstream errorMessage;
+        std::copy(shaderErrorMessage.begin(), shaderErrorMessage.end(),
+                  std::ostream_iterator<char>(errorMessage, ""));
+        
+        if(linkStatus == GL_FALSE)
+            throw std::runtime_error(errorMessage.str());
+        else
+            Logger::Instance()->critical(errorMessage.str());
+    }
+
+    glDetachShader(this->shaderProgramID, this->vertexShaderID);
+    glDetachShader(this->shaderProgramID, this->fragmentShaderID);
+
+    glDeleteShader(this->vertexShaderID);
+    glDeleteShader(this->fragmentShaderID);
+}
+
+Shader::~Shader() {
+
+    /* glDetachShader(this->shaderProgramID, this->vertexShaderID); */
+    /* glDetachShader(this->shaderProgramID, this->fragmentShaderID); */
+
+    /* glDeleteShader(this->vertexShaderID); */
+    /* glDeleteShader(this->fragmentShaderID); */
+}
+
+GLuint Shader::getProgramID() { return this->shaderProgramID; }
+
+void Shader::compileShader(GLuint& shaderID, std::string& shaderCode, 
+                           std::string logInfo) {
+
+    Logger::Instance()->info("Compiling shader: " + logInfo);
+
+    const char* shaderSource = shaderCode.c_str();
+    glShaderSource(shaderID, 1, &shaderSource, NULL);
+    /* std::unique_ptr<const char[]> shaderSource(shaderCode.c_str()); */
+    /* glShaderSource(shaderID, 1, shaderSource.get(), NULL); */
+    glCompileShader(shaderID);
+
+    GLint compileStatus = 0;
+    int infoLogLength = 0;
+
+    glGetShaderiv(shaderID, GL_COMPILE_STATUS, &compileStatus);
+    glGetShaderiv(shaderID, GL_INFO_LOG_LENGTH, &infoLogLength);
+
+    if(infoLogLength > 0) {
+
+        std::vector<char> shaderErrorMessage(infoLogLength + 1);
+        glGetShaderInfoLog(shaderID, infoLogLength, NULL,
+                           &shaderErrorMessage[0]);
+        std::stringstream errorMessage;
+        std::copy(shaderErrorMessage.begin(), shaderErrorMessage.end(),
+                  std::ostream_iterator<char>(errorMessage, ""));
+
+        if(compileStatus == GL_FALSE)
+            throw std::runtime_error(errorMessage.str());
+        else
+            Logger::Instance()->critical(errorMessage.str());
+    }
+
+}
+
+void Shader::linkShaders() {
+}
---
+++ src/main/Shader.h
@@ -0,0 +1,39 @@
+/**
+ * @file Shader.h
+ * @brief Describes a Shader
+ * @author Samuel Gauthier
+ * @version 0.1.0
+ * @date 2018-11-21
+ */
+
+#ifndef SHADER_H
+#define SHADER_H value
+
+#include <string>
+/* #include <GL/glew.h> */
+
+class Shader {
+
+public:
+
+    Shader() = delete;
+    Shader(std::string vertexShaderPath, std::string fragmentShaderPath);
+    ~Shader();
+
+    GLuint getProgramID();
+    
+private:
+
+    GLuint shaderProgramID;
+    GLuint vertexShaderID;
+    GLuint fragmentShaderID;
+
+    std::string vertexShaderPath;
+    std::string fragmentShaderPath;
+
+    void compileShader(GLuint& shaderID, std::string& shaderCode,
+                       std::string logInfo = "");
+    void linkShaders();
+};
+
+#endif /* ifndef SHADER_H */
